eed7a0c84691212ed0f61bbc296e0674842907d2
Duong Vo Tran <davidvotran22@gmail.com>
2021-09-24 20:15:39 +0700

    fix module names

--- cococaption/pycocoevalcap/eval.py
+++ cococaption/pycocoevalcap/eval.py
@@ -1,10 +1,10 @@
 __author__ = 'tylin'
 from .tokenizer.ptbtokenizer import PTBTokenizer
-from .bleu.bleu import Bleu
-from .meteor.meteor import Meteor
-from .rouge.rouge import Rouge
-from .cider.cider import Cider
-from .spice.spice import Spice
+from .bleu.Bleu import Bleu
+from .meteor.Meteor import Meteor
+from .rouge.Rouge import Rouge
+from .cider.Cider import Cider
+from .spice.Spice import Spice
 
 
 class COCOEvalCap:
